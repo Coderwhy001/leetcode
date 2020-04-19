2367a1700bbf5eb9c4cb599c5aa29f9a52e83db8
2268849815 <2268849815@qq.com>
2020-04-19 22:43:01 +0800

    4.19

---
+++ 统计重复个数/1.js
@@ -0,0 +1,18 @@
+var getMaxRepetitions = function(s1, n1, s2, n2) {
+  arr1 = s1.split("");
+  arr2 = s2.split("");
+  let index = 0;
+  let cnt = 0
+  for (let i = 0; i < n1; i++) {
+    for (let j = 0; j < s1.length; j++) {
+      if (arr1[j] == arr2[index]) {
+        index++;
+      }
+      if (index === arr2.length) {
+        index = 0;
+        cnt++;
+      }
+    }
+  }
+  return parseInt(cnt/n2)
+};
